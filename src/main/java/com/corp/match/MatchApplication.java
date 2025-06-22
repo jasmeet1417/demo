989afdca9afb9a37a0203eeb8425604c8c996207
Jasmeet Singh <jasmeetsingh1417@gmail.com>
2025-06-22 14:13:43 +0530

    Initial commit of CM-App-For-Corporates

---
+++ src/main/java/com/corp/match/MatchApplication.java
@@ -0,0 +1,14 @@
+package com.corp.match;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatchApplication {
+
+	public static void main(String[] args) {
+		System.out.println("corp match");
+		SpringApplication.run(MatchApplication.class, args);
+	}
+
+}
